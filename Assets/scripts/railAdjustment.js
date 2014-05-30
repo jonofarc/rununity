54463444c69b7de6a492f6fb5f3661041b8fc563
jonofarc <jmaldonadom1989@gmail.com>
2014-05-30 09:33:47 -0600

    agregados mas tipos de spawneos randomisaod spara los enemigos

---
+++ Assets/scripts/railAdjustment.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+public var secondsToWait: float;
+private var newTick: float;
+
+
+function Start () {
+secondsToWait=1f;
+newTick=Time.time+secondsToWait;
+}
+
+function Update () {
+
+
+if(newTick<Time.time){
+Debug.Log("entre");
+
+	if((this.transform.position.x>0)&&(this.transform.position.x<3)){
+
+		this.transform.position=new Vector3 (2,this.transform.position.y,this.transform.position.z);
+
+	}
+
+
+
+
+newTick=Time.time+secondsToWait;
+}//end of if(newTick>Time.time)
+
+
+
+}
--- Assets/scripts/spawnObjects.js
+++ Assets/scripts/spawnObjects.js
@@ -36,8 +36,42 @@ function Update () {
 }
 
 function BadSpawn(){
-var carril = Random.Range(1,4)*2;
-Instantiate (BadSpawneable, new Vector3(carril,myPlayer.transform.position.y+3,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+var carril: int;
+	var tipoEnemigo= Random.Range(1,7);
+
+	if(tipoEnemigo==1){
+		carril = 2;
+		Instantiate (BadSpawneable, new Vector3(carril,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+2,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+4,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+	}
+	if(tipoEnemigo==2){
+		carril = 2;
+		Instantiate (BadSpawneable, new Vector3(carril,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+4,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+	}
+	if(tipoEnemigo==3){
+		carril = Random.Range(1,4)*2;
+		Instantiate (BadSpawneable, new Vector3(carril,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+2,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+	}
+	if(tipoEnemigo==4){
+		carril = Random.Range(1,4)*2;
+		Instantiate (BadSpawneable, new Vector3(carril,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril,2.5,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+	}
+	if(tipoEnemigo==5){
+		carril = Random.Range(1,4)*2;
+		Instantiate (BadSpawneable, new Vector3(carril,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+2,2.5,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+4,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+	}
+		if(tipoEnemigo==5){
+		carril = Random.Range(1,4)*2;
+		Instantiate (BadSpawneable, new Vector3(carril,2.5,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+2,0.1,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+		Instantiate (BadSpawneable, new Vector3(carril+4,2.5,(myPlayer.transform.position.z+100)), BadSpawneable.transform.rotation);
+	}
 
 }
 function GoodSpawn(){
