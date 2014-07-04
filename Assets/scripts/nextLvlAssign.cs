e52e9211758cafa7fabf2ae887071e4e59061d13
jonofarc <jmaldonadom1989@gmail.com>
2014-07-03 19:17:03 -0600

    ajustado sublvladjuster
    ajustad script creador de sublvls que se desajusto xD

---
+++ Assets/scripts/nextLvlAssign.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextLvlAssign : MonoBehaviour {
+
+	public string myNextLvl="Start";
+	// Use this for initialization
+	void Start () {
+		
+		IntermediateLevel.setNextLevel(myNextLvl.ToString());
+		IntermediateLevel.setFailLevel(Application.loadedLevelName);
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
