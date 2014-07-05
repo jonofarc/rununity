da82eb8f37a834786b19c322384b43086bc0eb8b
Damian Velazquez <osdamv@gmail.com>
2014-07-04 19:19:56 -0600

    estilo

--- Assets/scripts/gui/BaseGui.cs
+++ Assets/scripts/gui/BaseGui.cs
@@ -5,9 +5,10 @@ using System;
 public class BaseGui : MonoBehaviour {
 
 
-	Texture2D[] backgrounds= new Texture2D[56];
+	Texture2D[] backgrounds= null;
 	protected Font font= null;
 	protected void Start () {
+		backgrounds= new Texture2D[56];
 		for (int x = 0; x<56; x++) {
 			string file= String.Format("GUI/menu/sec_inicio_{0:00000}",x);
 			backgrounds[x]=(Texture2D)Resources.Load(file);
@@ -19,11 +20,13 @@ public class BaseGui : MonoBehaviour {
 	} 
 	private int currentImage=0;
 	protected void OnGUI(){
-		if (currentImage >= backgrounds.Length)
-			currentImage = 0;
-		GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), backgrounds[currentImage]);
-		currentImage++;
-
+		if(backgrounds!=null){
+			if (currentImage >= backgrounds.Length)
+					currentImage = 0;
+				GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), backgrounds[currentImage]);
+				currentImage++;
+		}
+		GUI.skin.label.fontSize=22;
 		GUI.skin.font=font;
 		GUI.skin.button.fontSize=(int) (Screen.width * 0.04f);
 		GUI.skin.button.wordWrap=true;
--- Assets/scripts/gui/HUD.cs
+++ Assets/scripts/gui/HUD.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using System.IO;
 
-public class HUD : MonoBehaviour {
+public class HUD : BaseGui {
 	public GameObject player;//asignamos al player para regresarle valores cuadno la barra de hp este llena
 	public GUIStyle pointsColor;
 	public GUIStyle watermarkStyle;
@@ -88,13 +88,17 @@ public class HUD : MonoBehaviour {
 	}
 	float sizeRatio=.1f;
 	void OnGUI(){
+		base.OnGUI();
 		// jonathan agregado el puntaje y su estilo
-		pointsColor.normal.background = (Texture2D)Resources.Load("GUI/HUD/pointsMetter");
-		pointsColor.alignment= TextAnchor.MiddleCenter;
-		pointsColor.normal.textColor = Color.white;
-		GUI.Label(new Rect(10, 10, Screen.width*0.15f, Screen.height*0.13f), "puntos:"+points.ToString(),pointsColor);
-
-		GUI.Label(new Rect(Screen.width/2, 10, Screen.width*0.15f, Screen.height*0.13f), "sublvl:"+sublvl.ToString(),pointsColor);
+		Texture2D pointst = (Texture2D)Resources.Load("GUI/HUD/puntos");
+		GUI.DrawTexture(new Rect(10, 10, 196, 64),pointst );
+		GUI.Label(new Rect(20, 30, 128, 64),points.ToString());
+		/*"puntos:"+points.ToString(),points*/
+		Texture2D levelt = (Texture2D)Resources.Load("GUI/HUD/nivel");
+		GUI.DrawTexture(new Rect(210, 10, 64,64),levelt );
+		GUI.contentColor=Color.blue;
+		GUI.Label(new Rect(235, 30, 64,64),sublvl.ToString());
+		GUI.contentColor=Color.white;
 
 		//jonathan agregado el label de watermark y su estilo
 		watermarkStyle.normal.background = (Texture2D)Resources.Load("UserInterface/capture_button_normal_XHigh");
@@ -114,12 +118,13 @@ public class HUD : MonoBehaviour {
 				Time.timeScale = 0;
 				Sound.pauseTheme(gameObject);
 			}
-			float fullHP=Screen.height*.7f,
-				topHp=(Screen.height*.15f)+(fullHP-(fullHP*hp));
+			float fullHP=Screen.height*.69f,
+				topHp=(Screen.height*.205f)+(fullHP-(fullHP*hp));
+			Texture2D water_border = (Texture2D)Resources.Load("GUI/HUD/barra");
+			GUI.DrawTexture(new Rect(Screen.width*.05f,Screen.height*.2f, Screen.width*.09f, Screen.height*.7f), water_border);
 
-			GUI.Box(new Rect(Screen.width*.05f,Screen.height*.15f,Screen.width*.05f, fullHP ),"");
-			Texture2D water = (Texture2D)Resources.Load("GUI/HUD/water");
-			GUI.DrawTexture(new Rect(Screen.width*.05f,topHp, Screen.width*.05f, fullHP*hp), water);
+			Texture2D water = (Texture2D)Resources.Load("GUI/HUD/barra-azul");
+			GUI.DrawTexture(new Rect(Screen.width*.06f,topHp, Screen.width*.07f, fullHP*hp), water);
 
 
 
@@ -127,26 +132,26 @@ public class HUD : MonoBehaviour {
 		
 		if (Time.timeScale == 0) {
 			if (GUI.Button (new Rect (
-						(Screen.width / 2) - (Screen.width * .05f),
+						(Screen.width / 2) - (Screen.width * .3f),
 						(Screen.height / 2) - (Screen.height * .225f),
-						Screen.width * sizeRatio,
+						Screen.width * .6f,
 				Screen.height * sizeRatio), "Resumir")){
 				Sound.playTheme(gameObject);
 				Time.timeScale = 1;
 			} 
 				
 			if (GUI.Button (new Rect (
-						(Screen.width / 2) - (Screen.width * .05f),
+						(Screen.width / 2) - (Screen.width * .3f),
 						(Screen.height / 2) - (Screen.height * .075f),
-						Screen.width * sizeRatio,
+						Screen.width * .6f,
 						Screen.height * sizeRatio), "Reiniciar")) {
 				Application.LoadLevel(Application.loadedLevel);
 				Time.timeScale=1;
 			}				
 			if (GUI.Button (new Rect (
-						(Screen.width / 2) - (Screen.width * .05f),
+						(Screen.width / 2) - (Screen.width * .3f),
 						(Screen.height / 2) - (Screen.height * -.075f),
-						Screen.width * sizeRatio,
+						Screen.width * .6f,
 						Screen.height * sizeRatio), "Menu")){
 				Application.LoadLevel("Start");
 				Time.timeScale=1;
