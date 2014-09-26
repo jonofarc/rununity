0c9e7f301d27899d882787b76dda265c50185ef4
Damian Velazquez <osdamv@gmail.com>
2014-09-25 22:36:00 -0600

    logos juego

--- Assets/scripts/gui/Logos.cs
+++ Assets/scripts/gui/Logos.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public class Logos : MonoBehaviour {
 
 	// Use this for initialization
-	Texture2D luis;
-	Texture2D xholo;
+	Texture2D luis,xholo,logo1,logo2;
+
 	float init;
 	void Start () {
+		logo1=(Texture2D)Resources.Load("logos/logo1");
+		logo2=(Texture2D)Resources.Load("logos/logo2");
 		xholo=(Texture2D)Resources.Load("logos/xholo");
 		luis=(Texture2D)Resources.Load("logos/luis");
 	}
@@ -20,13 +22,16 @@ public class Logos : MonoBehaviour {
 		int left=(Screen.width/2)-128;
 		if(init==null)
 			init=Time.time;
-		if((init+4)>Time.time)
-			GUI.DrawTexture (new Rect (left,top,256, 256), luis, ScaleMode.StretchToFill);
-		else if((init+8)>Time.time)
-			GUI.DrawTexture (new Rect (left, top, 256, 256), xholo, ScaleMode.StretchToFill);
+		if((init+3)>Time.time)
+			GUI.DrawTexture (new Rect (left,top,256, 256), logo1, ScaleMode.StretchToFill);
+		else if((init+6)>Time.time)
+			GUI.DrawTexture (new Rect (left, top, 256, 256), logo2, ScaleMode.StretchToFill);
+		else if ((init+9)>Time.time)
+			GUI.DrawTexture (new Rect ((Screen.width/2)-240, top, 480, 256), luis, ScaleMode.StretchToFill);
 		else if ((init+12)>Time.time)
+			GUI.DrawTexture (new Rect (left, top, 256, 256), xholo, ScaleMode.StretchToFill);
+		else if ((init+15)>Time.time)
 			Application.LoadLevel("VideoIntro");
-		
 		GUILayout.FlexibleSpace();
 		GUILayout.EndHorizontal();
 		GUILayout.FlexibleSpace();
